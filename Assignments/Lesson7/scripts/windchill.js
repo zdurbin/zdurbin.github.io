9047658cdd8d84f8ad5a2a6297382ae3345766d9
zdurbin <dur15014@byui.edu>
2018-02-25 12:15:10 -0800

    WindChill working

--- Assignments/Lesson7/scripts/windchill.js
+++ Assignments/Lesson7/scripts/windchill.js
@@ -1,18 +1,18 @@
-document.getElementById("windchill").innerHTML = wc; 
+var wch = doWindChill();
 
-var wch = doWindChill();  
+document.getElementById("windchill").innerHTML = wch;  
 
 function doWindChill() {
 
-    var h = parseInt(document.getElementById("hi").value);
+    var h = 83 //parseInt(document.getElementById("hi").value);
 
-    var l = parseInt(document.getElementById("low").value);
+    var l = 60 //parseInt(document.getElementById("low").value);
 
 
     var temp = (h + l) / 2;
 
 
-    var speed = parseInt(document.getElementById("speed").value);
+    var speed = 5 //parseInt(document.getElementById("speed").value);
 
     s = Math.pow(speed, 0.16);
 
@@ -26,6 +26,7 @@ function windChill(t, s){
 
     return 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
 
+    
 }
 
 function get(windChill) {

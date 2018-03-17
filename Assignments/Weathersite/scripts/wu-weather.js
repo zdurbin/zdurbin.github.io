52b3328ef8d811c278867b60c1eb4c2f269977c4
zdurbin <dur15014@byui.edu>
2018-03-16 21:29:46 -0700

    Readability cleanup

--- Assignments/Weathersite/scripts/wu-weather.js
+++ Assignments/Weathersite/scripts/wu-weather.js
@@ -1,88 +1,43 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/MN/Franklin.json' , true);
+weatherObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/MN/Franklin.json', true);
 weatherObject.send();
 
-weatherObject.onload = function() {
+weatherObject.onload = function () {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
-    
+
     document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('rain').innerHTML = weatherInfo.current_observation.precip_1hr_in;
     document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
     document.getElementById('speed').innerHTML = weatherInfo.current_observation.wind_mph;
-    
+
     var icon_path = weatherInfo.current_observation.icon_url;
     var urlString = document.location.href;
     console.log(urlString);
     var found = urlString.indexOf("https");
     console.log(found);
-    
-    if(found>=0){
+
+    if (found >= 0) {
         icon_path = icon_path.replace("http", "https");
     }
-    
+
     document.getElementById('w_icon').src = icon_path;
-    
+
 }
 
 var forcastObject = new XMLHttpRequest();
 forcastObject.open('GET', '//api.wunderground.com/api/4326409d402b76d5/forecast/q/MN/Franklin.json', true);
 forcastObject.send();
 
-forcastObject.onload = function(){
-    var forcastInfo= JSON.parse(forcastObject.responseText);
+forcastObject.onload = function () {
+    var forcastInfo = JSON.parse(forcastObject.responseText);
     console.log(forcastInfo);
-    
+
     document.getElementById('hi').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
     document.getElementById('low').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
     document.getElementById('conditions').innerHTML = forcastInfo.forecast.simpleforecast.forecastday["0"].conditions;
     document.getElementById('forcast').innerHTML = forcastInfo.forecast.txt_forecast.forecastday["0"].fcttext;
-    
+
 }
 
 //END FRANKLIN//
-
-
-//START SPRINGFIELD//
-var weatherObjectB = new XMLHttpRequest();
-weatherObjectB.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/OR/Springfield.json' , true);
-weatherObjectB.send();
-
-weatherObjectB.onload = function() {
-    var weatherInfoB = JSON.parse(weatherObjectB.responseText);
-    console.log(weatherInfoB);
-    
-    
-    document.getElementById('currentTempB').innerHTML = weatherInfoB.current_observation.temp_f;
-    document.getElementById('rainB').innerHTML = weatherInfoB.current_observation.precip_1hr_in;
-    document.getElementById('windchillB').innerHTML = weatherInfoB.current_observation.windchill_f;
-    document.getElementById('speedB').innerHTML = weatherInfoB.current_observation.wind_mph;
-    
-    var icon_path = weatherInfoB.current_observation.icon_url;
-    var urlString = document.location.href;
-    console.log(urlString);
-    var found = urlString.indexOf("https");
-    console.log(found);
-    
-    if(found>=0){
-        icon_path = icon_path.replace("http", "https");
-    }
-    
-    document.getElementById('w_iconB').src = icon_path;
-    
-}
-
-var forcastObjectB = new XMLHttpRequest();
-forcastObjectB.open('GET', '//api.wunderground.com/api/4326409d402b76d5/forecast/q/OR/Springfield.json', true);
-forcastObjectB.send();
-
-forcastObjectB.onload = function(){
-    var forcastInfoB= JSON.parse(forcastObjectB.responseText);
-    console.log(forcastInfoB);
-    
-    document.getElementById('hiB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
-    document.getElementById('lowB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
-    document.getElementById('conditionsB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].conditions;
-    document.getElementById('forcastB').innerHTML = forcastInfoB.forecast.txt_forecast.forecastday["0"].fcttext;
-    
-}
--- Assignments/Weathersite/scripts/wu-weatherb.js
+++ Assignments/Weathersite/scripts/wu-weatherb.js
@@ -1,43 +1,43 @@
 //START SPRINGFIELD//
 var weatherObjectB = new XMLHttpRequest();
-weatherObjectB.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/OR/Springfield.json' , true);
+weatherObjectB.open('GET', '//api.wunderground.com/api/4326409d402b76d5/conditions/q/OR/Springfield.json', true);
 weatherObjectB.send();
 
-weatherObjectB.onload = function() {
+weatherObjectB.onload = function () {
     var weatherInfoB = JSON.parse(weatherObjectB.responseText);
     console.log(weatherInfoB);
-    
-    
+
+
     document.getElementById('currentTempB').innerHTML = weatherInfoB.current_observation.temp_f;
     document.getElementById('rainB').innerHTML = weatherInfoB.current_observation.precip_1hr_in;
     document.getElementById('windchillB').innerHTML = weatherInfoB.current_observation.windchill_f;
     document.getElementById('speedB').innerHTML = weatherInfoB.current_observation.wind_mph;
-    
+
     var icon_path = weatherInfoB.current_observation.icon_url;
     var urlString = document.location.href;
     console.log(urlString);
     var found = urlString.indexOf("https");
     console.log(found);
-    
-    if(found>=0){
+
+    if (found >= 0) {
         icon_path = icon_path.replace("http", "https");
     }
-    
+
     document.getElementById('w_iconB').src = icon_path;
-    
+
 }
 
 var forcastObjectB = new XMLHttpRequest();
 forcastObjectB.open('GET', '//api.wunderground.com/api/4326409d402b76d5/forecast/q/OR/Springfield.json', true);
 forcastObjectB.send();
 
-forcastObjectB.onload = function(){
-    var forcastInfoB= JSON.parse(forcastObjectB.responseText);
+forcastObjectB.onload = function () {
+    var forcastInfoB = JSON.parse(forcastObjectB.responseText);
     console.log(forcastInfoB);
-    
+
     document.getElementById('hiB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
     document.getElementById('lowB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
     document.getElementById('conditionsB').innerHTML = forcastInfoB.forecast.simpleforecast.forecastday["0"].conditions;
     document.getElementById('forcastB').innerHTML = forcastInfoB.forecast.txt_forecast.forecastday["0"].fcttext;
-    
+
 }
